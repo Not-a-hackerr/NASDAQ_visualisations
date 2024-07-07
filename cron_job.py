723e91fd1c9a4df0198d76af6728fa311706b896
Nathan Mwasuku <89474316+Not-a-hackerr@users.noreply.github.com>
2024-07-07 18:25:59 +0000

    Cobmines the crone jobs into one job script

--- cron_job.py
+++ cron_job.py
@@ -5,6 +5,7 @@ import yfinance as yf
 from datetime import timedelta
 import psycopg2.extras as extras
 import streamlit as st
+import pandas as pd
 
 # Web Scrap to get all 100 NASDAQ companies and their corresponding tickers
 url = 'https://stockanalysis.com/list/nasdaq-100-stocks/'
@@ -23,11 +24,12 @@ SQL_User = st.secrets['SQLUser']
 SQL_Pass = st.secrets['SQLPass']
 Host = st.secrets['Host']
 
+
 def fetch_new_data(ticker, last_retrieval_datetime):
     start_datetime = last_retrieval_datetime + timedelta(hours=1)
-    
-    
+
     ticker_obj = yf.Ticker(ticker)
+    
     new_data = ticker_obj.history(start=start_datetime, interval='1h')
     return new_data
 
@@ -91,9 +93,75 @@ def update_db_new_value(ticker, last_retrieval_time):
     new_data = cleaning_data(new_data)
     
     return updated_db(conn, new_data, ticker)
-    
+
 if __name__ =="__main__":
     last_time_updated = last_updated()
     for i in NASDAQ:
         update_db_new_value(i, last_time_updated)
+
+
+
+def execute_values(conn, df, table):
+    tuples = [tuple(x) for x in df.to_numpy()]
+    cols = ','.join(list(df.columns))
+    
+    query = f"INSERT INTO {table}({cols}) VALUES %s"
+    cursor = conn.cursor()
+    try:
+        extras.execute_values(cursor, query, tuples)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error:", error)
+        conn.rollback()
+        cursor.close()
+        return 1
+    print("execute_values() done")
+    cursor.close()
+
+def replace_table(conn, table_ticker, df):
+    cursor = conn.cursor()
+
+    drop_sql = f"DROP TABLE IF EXISTS student.nm_{table_ticker};"
+    cursor.execute(drop_sql)
+    conn.commit()
+
+    create_sql = f"""
+    CREATE TABLE student.nm_{table_ticker} ( 
+        ticker VARCHAR(10), 
+        name VARCHAR(50),
+        market_cap BIGINT,
+        sector VARCHAR(50), 
+        industry VARCHAR(50), 
+        market_cap_billions REAL
+    );
+    """
+    cursor.execute(create_sql)
+    conn.commit()
+
+    return execute_values(conn, df, f'student.nm_{table_ticker}')
+
+def get_company_info(ticker_symbol):
+    stock = yf.Ticker(ticker_symbol)
+    info = stock.info
+    return {
+        'ticker': ticker_symbol,
+        'name': info.get('longName'),
+        'market_cap': info.get('marketCap'),
+        'sector': info.get('sector'),
+        'industry': info.get('industry')
+    }
+
+company_data = [get_company_info(ticker) for ticker in NASDAQ]
+df = pd.DataFrame(company_data)
+df['market_cap_billions'] = df['market_cap'] / 1e9
+
+conn = psycopg2.connect(database='pagila',
+                        user= SQL_User,
+                        host= Host,
+                        password= SQL_Pass,
+                        port=5432
+)
+    
+if __name__ =="__main__":
+    replace_table(conn, 'nasdaq_info', df)
 
